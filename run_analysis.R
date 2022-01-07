93ea089492f60df9060ccf3743ccf34abf6faeb6
Rachel Spicer <rachelaspicer@gmail.com>
2022-01-07 13:12:08 +0000

    reorder code

--- run_analysis.R
+++ run_analysis.R
@@ -6,18 +6,19 @@ source("./project_support.R")
 # Make figures folder
 make.dir("figures")
 
+# Find questions that discriminate between clusters
+make.dir("./02_clusters/input")
+files <- c("./01_make_nexus/output/mystery_cults.csv", "./01_make_nexus/output/question_dict.csv", "./data/BEAST/mystery_cults.tree")
+file.copy(files, "./02_clusters/input", overwrite = TRUE)
+setwd("./02_clusters/")
+source("clusters.R")
+setwd("..")
+
 # Visualize tree
-make.dir("./02_visualization/input")
-files <- c( "./01_make_nexus/output/mystery_cults.csv", "./data/BEAST/mystery_cults.tree")
-file.copy(files, "./02_visualization/input", overwrite = TRUE)
-setwd("./02_visualization/")
+make.dir("./03_visualization/input")
+files <- c( "./02_clusters/output/data_cluster.csv", "./data/BEAST/mystery_cults.tree")
+file.copy(files, "./03_visualization/input", overwrite = TRUE)
+setwd("./03_visualization/")
 source("visualization.R")
 setwd("..")
 
-# Find questions that discriminate between clusters
-make.dir("./03_clusters/input")
-files <- c("./01_make_nexus/output/mystery_cults.csv", "./01_make_nexus/output/question_dict.csv", "./data/BEAST/mystery_cults.tree")
-file.copy(files, "./03_clusters/input", overwrite = TRUE)
-setwd("./03_clusters/")
-source("clusters.r")
-setwd("..")
